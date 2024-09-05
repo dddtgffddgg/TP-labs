ed98227c2e068ea39cf183196d0e843b1bbfa7a6
popi popi po po pi po <diana.bakieva69@gmail.com>
2024-09-05 09:36:04 +0500

    Update arcanoid_on_C.c

--- lab1_game/arcanoid_on_C.c
+++ lab1_game/arcanoid_on_C.c
@@ -1,12 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+// on c#
+#include <iostream>
 
-#define screen_x 60
-#define screen_y 20
+int main() 
+{
+    // INIT VALUE
+    bool rungame = true;
+    int temp_rungame = 20;
+    int width = 50;
+    int height = 20;
 
-char map[screen_y][screen_x];
+    while (rungame) 
+    {
+        //render
+        system("cls")
+        for (int col = 0; col <= height; col++)
+        {
+            for (int row =0; row <= width; row++) 
+            {
+                if (row == 0 || col == 0 || row = width || col == height) 
+                {
+                    std::count << 'x';
+                }
+                else 
+                {
+                    std::count << ' ';
+                }
+            }
+        }
+        temp_rungame--;
+        if (!temp_rungame) 
+        {
+            rungame = false;
+        }
+    }
 
-int score = 0;
-int max_score = 0;
-
+}
